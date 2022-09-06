56edb6f94508fa446ed24a5ad9e26c82ff8ee308
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-09-06 07:55:25 +0000

    [Update] user機能作成(仮)

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_status, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +25,12 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def user_status
+    @user = User.find_by(name: params[:user][:name])
+    return if !@user
+    if (@user.valid_password?(params[:user][:password]) && (@user.user_status == true))
+      redirect_to new_user_registration_path
+    end
+  end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,17 +1,32 @@
 class Public::UsersController < ApplicationController
 
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to users_my_page_path
   end
 
   def out_check
+    @user = current_user
   end
 
   def out
+    @user = current_user
+    @user.update(user_params)
+    session[:current_customer] = nil
+    redirect_to  root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :zip_code, :address, :phone_number, :email, :user_status)
   end
 end
