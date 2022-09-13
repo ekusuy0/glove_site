f9138d3362616c2fc09778bede883e5af2f78c1f
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-09-13 14:47:14 +0000

    [Update] カートアイテムに追加・削除機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,24 +5,29 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.user_id = current_user.id
-    cart_item.save
-    redirect_to cart_items_path
+    cart_item = current_user.cart_items.new(cart_item_params)
+    if cart_item.save
+      redirect_to cart_items_path
+    else
+      redirect_to request.referer
+    end
+    logger.debug cart_item.errors.inspect
   end
 
   def update
   end
 
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
   end
 
   private
-
   def cart_item_params
-    params.require(:cart_item).permit(:quantity, :item_id)
+    params.require(:cart_item).permit(:item_id, :quantity)
   end
 end
--- app/models/ordered_item.rb
+++ app/models/ordered_item.rb
@@ -1,6 +1,6 @@
 class OrderedItem < ApplicationRecord
 
   belongs_to :order
-  belongs_to :custom_item
+  # belongs_to :custom_item
   belongs_to :item
 end
