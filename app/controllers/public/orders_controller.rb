bd6d3361c594066c27d57c082585470c3c953ca0
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-09-15 04:20:15 +0000

    [Update] 注文確定処理作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,20 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    order = current_user.orders.new(order_params)
+    order.save
+
+    current_user.cart_items.each do |cart_item|
+      ordered_item = OrderedItem.new
+      ordered_item.order_id = order.id
+      ordered_item.item_id = cart_item.item_id
+      ordered_item.quantity = cart_item.quantity
+      ordered_item.tax_price = cart_item.item.tax_price
+      ordered_item.save
+    end
+
+    current_user.cart_items.destroy_all
+    redirect_to complete_orders_path
   end
 
   def show
@@ -44,6 +58,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :name, :zip_code, :address)
+    params.require(:order).permit(:payment_method, :name, :zip_code, :address, :items_total, :delivery_money, :order_status)
   end
 end
