7164fffcd11b42554edd53cecda8a5e08ae9169a
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-09-16 08:19:06 +0000

    [Update] Admin側のユーザー編集機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,8 +9,18 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to admin_user_path(user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :address, :zip_code, :phone_number, :user_status, :email)
   end
 end
