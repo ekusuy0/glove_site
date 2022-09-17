cae4b43393e7ceb1b58d87b8933b64065f349ff0
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-09-17 16:10:32 +0000

    [Update] admin側注文履歴詳細画面作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,24 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
+    @ordered_items = @order.ordered_items
   end
 
   def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    order.ordered_items.each do |ordered_item|
+      if ordered_item.production_status == "not_startable"
+        ordered_item.update(production_status: "waiting_for_production")
+      end
+    end
+    redirect_to admin_order_path(order.id)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
