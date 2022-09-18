0ca6b4b476ebb8ee84cada03402c71ccd3c6edeb
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-09-18 05:04:51 +0000

    [Update] Admin側注文詳細画面ステータスの連動

--- app/controllers/admin/ordered_items_controller.rb
+++ app/controllers/admin/ordered_items_controller.rb
@@ -1,5 +1,28 @@
 class Admin::OrderedItemsController < ApplicationController
 
   def update
+    ordered_item = OrderedItem.find(params[:id])
+    ordered_item.update(ordered_item_params)
+    ordered_items = OrderedItem.where(order_id: ordered_item.order_id)
+    count = 0
+    ordered_items.each do |ordered_item|
+      if ordered_item.production_status == "production_completed"
+        count = count + 1
+      end
+    end
+    order = ordered_item.order
+    if ordered_item.production_status == "in_production"
+      order.update(order_status: "in_production")
+    end
+    if ordered_items.length == count
+      order.update(order_status: "preparing_to_ship")
+    end
+    redirect_to request.referer
+  end
+
+  private
+
+  def ordered_item_params
+    params.require(:ordered_item).permit(:production_status)
   end
 end
